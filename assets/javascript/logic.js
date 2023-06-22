5f423f843167b661c4ef82043f149b123be099e6
Kris Kurzawa <kurzawa1@yahoo.com>
2023-06-22 18:19:43 -0400

    updated HTML/CSS/JavaScrpt

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,6 +1,7 @@
 //start page variables
 var button = document.querySelector('#begin');
-var homePage= document.querySelector("#homePage")
+var homePage= document.querySelector("#homePage");
+var submitButton = document.querySelector("#submitButton");
 
 //question/answer variables
 var btn1 = document.querySelector("#ans1");
@@ -22,19 +23,20 @@ var timerInterval ;
 var index=0;
 var score=0;
 
+
 //game start 
 function startQuiz(){
 homePage.classList.add("hide") 
 questionCont.classList.replace("hide", "show")
 
-if(index===questions.length){
+if(index >= questions.length){
     endQuiz()
 }
-questionEL.textContent=questions[index].question
-btn1.textContent=questions[index].option1
-btn2.textContent=questions[index].option2
-btn3.textContent=questions[index].option3
-btn4.textContent=questions[index].option4
+questionEL.textContent=questions[index].question;
+btn1.textContent=questions[index].option1;
+btn2.textContent=questions[index].option2;
+btn3.textContent=questions[index].option3;
+btn4.textContent=questions[index].option4;
 }
 
 //function for timer
@@ -50,11 +52,13 @@ function setTime() {
     }, 1000);
   }
 
+// checks if answer is correct or incorrect and adds to score or deducts time
 function checkAnswer(answer){
     if(answer === questions[index].correct){
         index++
         score+=10
         document.getElementById('correct').style.display = 'flex'
+        console.log(score);
         setTimeout (function() {
             startQuiz()
             document.getElementById('correct').style.display = 'none'
@@ -64,6 +68,7 @@ function checkAnswer(answer){
         index++;
         secondsLeft-=10;
         document.getElementById('incorrect').style.display = 'flex'
+        console.log(score);
         setTimeout (function() {
             startQuiz()
             document.getElementById('incorrect').style.display = 'none'
@@ -86,7 +91,33 @@ button.addEventListener("click", ()=>{
     setTime();
 });
 
-btnGrid.addEventListener("click", ()=>{
+
+btnGrid.addEventListener("click", (event)=>{
     var btnClick= event.target.textContent
     checkAnswer(btnClick);
-})
+});
+
+submitButton.addEventListener("click", function() {
+    localStorage.setItem("score", score)
+    userContainer.classList.replace("show", "hide")
+    document.getElementById("highScoresDisplay").style.display = "flex";
+    var initials = document.getElementById("initials").value;
+    console.log(initials);
+  });
+
+
+
+  // submitButton.addEventListener("click", function() {
+//   localStorage.getItem(score)
+//   highScoresDisplay = score;
+// });
+
+
+// possible assets for local storage
+
+// if (email === "") {
+//     displayMessage("error", "Email cannot be blank");
+//   } else if (password === "") {
+//     displayMessage("error", "Password cannot be blank");
+//   } else {
+//     displayMessage("success", "Registered successfully");
